89e0db3a38a78120d88c5ad4e1efcdd447d02f80
ganeshi4u <ganeshvarmai4u@gmail.com>
2019-12-12 21:35:22 +0530

    Utils: Pass our ziptype prop as parameter to server url
    - This is to handle and serve responses separately for vanilla and
      gapps builds.
    
    Change-Id: I553f279a821a8be2c8879f92e83d72157287e923
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- app/src/main/java/org/palladiumos/updater/misc/Constants.java
+++ app/src/main/java/org/palladiumos/updater/misc/Constants.java
@@ -47,6 +47,7 @@ public final class Constants {
     public static final String PROP_RELEASE_TYPE = "ro.palladium.buildtype";
     public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "palladium.updater.allow_downgrading";
     public static final String PROP_UPDATER_URI = "palladium.updater.uri";
+    public static final String PROP_ZIP_TYPE = "ro.palladium.ziptype";
 
     public static final String PREF_INSTALL_OLD_TIMESTAMP = "install_old_timestamp";
     public static final String PREF_INSTALL_NEW_TIMESTAMP = "install_new_timestamp";
